b7284cb8e8c27d2c8fcefb42ec9bad01013d9e10
OmoBe <summersun1978@live.co.uk>
2018-08-21 14:17:49 +0100

    Add files via upload

---
+++ globalkeypad/KeyPad.xaml.cs
@@ -0,0 +1,58 @@
+﻿using globalkeypad.Keys;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace globalkeypad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<KeypadKey> _mpKeypad;
+        private const decimal _keypressTime = 0.100m;
+        private const decimal _keypressPause = 0.500m;
+
+        private decimal _userkpTime;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _mpKeypad = KeypadKey.MobilePhoneKeypad;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string _input = txtInputString.Text.ToLowerInvariant();
+            StringBuilder _keySequence = new StringBuilder();
+            //sb.Append(c);
+            _userkpTime = 0.0m;
+            int _dpp = -1;
+            _keySequence.Clear();
+
+            //enumerate each character in the input string
+            foreach (char c in _input)
+            {
+                foreach (KeypadKey kk in _mpKeypad)
+                {
+                    //search for the input character in the keypad character lists
+                    if(kk.AlphabeticKeys.Exists(_c => _c.Equals(c)))
+                    {
+                        int _dp = _mpKeypad.IndexOf(kk);
+                        if (_dpp == _dp)
+                            _userkpTime = _userkpTime + _keypressPause;
+
+                        _userkpTime = _userkpTime + _keypressTime;
+                        for(int i = 0; i<=kk.AlphabeticKeys.IndexOf(c); i++)
+                            _keySequence.Append(kk.NumericKey);
+                       
+                        _dpp = _dp;
+                        break;
+                    }
+                }
+            }
+            lbOut.Content = _userkpTime.ToString("0.##") + " second(s)";
+            lbSequenceOut.Content = _keySequence.ToString();
+        }
+    }
+}
---
+++ globalkeypad/KeypadKey.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace globalkeypad.Keys
+{
+    public class KeypadKey
+    {
+        public char NumericKey { get; set; }
+        //public char AlphabeticKey { get; set;  } 
+        public List<char> AlphabeticKeys { get; private set; }
+
+
+        public static List<KeypadKey> MobilePhoneKeypad
+        {
+            get
+            {
+                var _mpk = new List<KeypadKey>
+                {
+                    new KeypadKey() {NumericKey = '0',  AlphabeticKeys = new List<char>() {' ' } },
+                    new KeypadKey() {NumericKey = '1',  AlphabeticKeys = new List<char>() },
+                    new KeypadKey() {NumericKey = '2',  AlphabeticKeys = new List<char>() {'a', 'b', 'c'} },
+                    new KeypadKey() {NumericKey = '3',  AlphabeticKeys = new List<char>() {'d', 'e', 'f'} },
+                    new KeypadKey() {NumericKey = '4',  AlphabeticKeys = new List<char>() {'g', 'h', 'i'} },
+                    new KeypadKey() {NumericKey = '5',  AlphabeticKeys = new List<char>() {'j', 'k', 'l'} },
+                    new KeypadKey() {NumericKey = '6',  AlphabeticKeys = new List<char>() {'m', 'n', 'o'} },
+                    new KeypadKey() {NumericKey = '7',  AlphabeticKeys = new List<char>() {'p', 'q', 'r', 's'} },
+                    new KeypadKey() {NumericKey = '8',  AlphabeticKeys = new List<char>() {'t', 'u', 'v'} },
+                    new KeypadKey() {NumericKey = '9',  AlphabeticKeys = new List<char>() {'w', 'x', 'y', 'z'} },
+                    new KeypadKey() {NumericKey = '*',  AlphabeticKeys = new List<char>() },
+                    new KeypadKey() {NumericKey = '#',  AlphabeticKeys = new List<char>() },
+                };
+
+                return _mpk;
+            }
+        }
+    }
+}
